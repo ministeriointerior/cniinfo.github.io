3b9f0d80e608b128e94f3650e6be539161f68893
ministeriointerior <ministeriiodelinterior@gmail.com>
2025-11-17 19:14:34 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 function login() {
   const password = document.getElementById("password").value;
 
-  if (password === "918097552505724968") {
+  if (password === "1010520491292897300") {
     window.location.href = "access.html";
   } else {
     alert(" ⨸ ﻿ＡＣＣＥＳＯ　ＤＥＮＥＧＡＤＯ．　　　ＮＯ　ＩＮＴＥＮＴＥＳ　ＡＣＣＥＤＥＲ　ＤＯＮＤＥ　ＮＯ　ＥＳＴＡＳ　ＡＵＴＯＲＩＺＡＤＯ ⨸．");
   }
 }
+
 
 
 
