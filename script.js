e89c586f6631475c68aaab7d52d9a9fde21508c8
ministeriointerior <ministeriiodelinterior@gmail.com>
2025-11-17 19:10:44 +0100

    Change login password for access validation

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 function login() {
   const password = document.getElementById("password").value;
 
-  if (password === "1432828684317753355") {
+  if (password === "918097552505724968") {
     window.location.href = "access.html";
   } else {
     alert(" ⨸ ﻿ＡＣＣＥＳＯ　ＤＥＮＥＧＡＤＯ．　　　ＮＯ　ＩＮＴＥＮＴＥＳ　ＡＣＣＥＤＥＲ　ＤＯＮＤＥ　ＮＯ　ＥＳＴＡＳ　ＡＵＴＯＲＩＺＡＤＯ ⨸．");
   }
 }
+
 
 
 
